23f28dd4ff463c533a9cd4e3c578175435a2612c
wesllei.ferreira <wesllei.gustavo@gmail.com>
2021-05-21 09:51:46 -0300

    Ajustes teste HealthCheckIT

--- src/test/java/br/com/devwell/task/prod/HealthCheckIT.java
+++ src/test/java/br/com/devwell/task/prod/HealthCheckIT.java
@@ -3,9 +3,8 @@ package br.com.devwell.task.prod;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -15,10 +14,8 @@ import java.util.concurrent.TimeUnit;
 public class HealthCheckIT {
 
     @Test
-    public void healthCheckTest() throws MalformedURLException {
-        Capabilities capbilities = DesiredCapabilities.chrome();
-        WebDriver webDriver =new RemoteWebDriver(new URL("http://192.168.10.20:4444/wd/hub"),capbilities);
-
+    public void healthCheckTestIT() throws MalformedURLException {
+        WebDriver webDriver =new RemoteWebDriver(new URL("http://192.168.10.20:4444/wd/hub"),new ChromeOptions());
         try{
             webDriver.navigate().to("http://192.168.10.20:9999/tasks");
             webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
